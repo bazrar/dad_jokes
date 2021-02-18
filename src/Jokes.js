d7e192622fb9158a897bf8e61ee2d4d2ad669734
varbuzz <buzz.rejan@gmail.com>
2021-02-19 04:24:55 +0545

    added spinner

--- src/Jokes.js
+++ src/Jokes.js
@@ -12,8 +12,9 @@ export default class Jokes extends Component {
     super(props);
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes")) || [],
+      loading: false,
     };
-    this.getJokes = this.getJokes.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     // console.log("cdm");
@@ -33,9 +34,10 @@ export default class Jokes extends Component {
       //   console.log(res.data);
       jokes.push({ joke: res.data.joke, votes: 0, id: res.data.id });
     }
-    this.setState({ jokes: jokes });
+    this.setState({ jokes: jokes, loading: false });
     window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
+
   handleVote(id, delta) {
     this.setState(
       (st) => ({
@@ -47,36 +49,50 @@ export default class Jokes extends Component {
         window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
     );
   }
+
+  handleClick() {
+    this.setState({ loading: true });
+    this.getJokes();
+  }
   render() {
-    return (
-      <div className="jokeList">
-        <div className="jokeList-sidebar">
-          <h1 className="jokeList-title">
-            <span>Dad</span>jokes
-            <img
-              src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/271/face-with-tears-of-joy_1f602.png"
-              alt="emoji"
-            />
-          </h1>
-          <button onClick={this.getJokes} className="jokeList-btn">
-            new jokes
-          </button>
+    if (this.state.loading) {
+      return (
+        <div className="spinner">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="jokeList-title">loading ...</h1>
         </div>
-        <div className="jokeList-jokes">
-          {this.state.jokes.map((joke) => {
-            return (
-              <Joke
-                key={joke.id}
-                id={joke.id}
-                votes={joke.votes}
-                text={joke.joke}
-                downvote={() => this.handleVote(joke.id, -1)}
-                upvote={() => this.handleVote(joke.id, 1)}
+      );
+    } else {
+      return (
+        <div className="jokeList">
+          <div className="jokeList-sidebar">
+            <h1 className="jokeList-title">
+              <span>Dad</span>jokes
+              <img
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/271/face-with-tears-of-joy_1f602.png"
+                alt="emoji"
               />
-            );
-          })}
+            </h1>
+            <button onClick={this.handleClick} className="jokeList-btn">
+              new jokes
+            </button>
+          </div>
+          <div className="jokeList-jokes">
+            {this.state.jokes.map((joke) => {
+              return (
+                <Joke
+                  key={joke.id}
+                  id={joke.id}
+                  votes={joke.votes}
+                  text={joke.joke}
+                  downvote={() => this.handleVote(joke.id, -1)}
+                  upvote={() => this.handleVote(joke.id, 1)}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
